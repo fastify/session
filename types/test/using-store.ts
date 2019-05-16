c41658097a39525498cf3a40c38d5fa7005f8216
3846masa <3846masahiro+git@gmail.com>
2019-05-16 14:47:07 +0900

    Add type tests

---
+++ types/test/using-store.ts
@@ -0,0 +1,23 @@
+import fastify from 'fastify';
+import cookie from 'fastify-cookie';
+import session from '../../';
+
+class EmptyStore {
+  set(_sessionId: string, _session: any, _callback: Function) {}
+
+  get(_sessionId: string, _callback: Function) {}
+
+  destroy(_sessionId: string, _callback: Function) {}
+}
+
+const app = fastify();
+
+app.register(cookie);
+app.register(session, {
+  secret: 'ABCDEFGHIJKLNMOPQRSTUVWXYZ012345',
+  store: new EmptyStore(),
+});
+
+app.listen(4000, '0.0.0.0', (_err, address) => {
+  console.log(address);
+});
