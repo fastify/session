08f1e166048a9c46fb4a7d70d6ffa58a4c173cf3
Vincent LE GOFF <vince.legoff@gmail.com>
2021-09-13 10:08:26 +0200

    feat: missing assertions

--- types/types.test-d.ts
+++ types/types.test-d.ts
@@ -7,10 +7,35 @@ import fastify, {
   FastifyReply,
 } from "fastify";
 
+class EmptyStore {
+  set(_sessionId: string, _session: any, _callback: Function) {}
+
+  get(_sessionId: string, _callback: Function) {}
+
+  destroy(_sessionId: string, _callback: Function) {}
+}
+
 const app: FastifyInstance = fastify();
 app.register(session);
 app.register(session, { secret: "DizIzSecret" });
 app.register(session, { secret: "DizIzSecret", rolling: true });
+app.register(session, {
+  secret: "ABCDEFGHIJKLNMOPQRSTUVWXYZ012345",
+  rolling: false,
+  cookie: {
+    secure: false,
+  },
+});
+app.register(session, {
+  secret: "ABCDEFGHIJKLNMOPQRSTUVWXYZ012345",
+  cookie: {
+    secure: false,
+  },
+});
+app.register(session, {
+  secret: "ABCDEFGHIJKLNMOPQRSTUVWXYZ012345",
+  store: new EmptyStore(),
+});
 expectError(app.register(session, {}));
 
 app.get("/", async (request, reply) => {
@@ -18,4 +43,6 @@ app.get("/", async (request, reply) => {
   expectType<FastifyReply>(reply);
   expectType<Session>(request.session);
   expectType<Readonly<session.SessionStore>>(request.sessionStore);
+  expectError((request.sessionStore = null));
+  expectError(request.session.doesNotExist());
 });
