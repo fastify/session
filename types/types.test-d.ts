e150f10c7534f7bab7396e48d58e29780f6d9c42
darkgl0w <31093081+darkgl0w@users.noreply.github.com>
2022-03-01 18:24:09 +0100

    test (types): using a callback w/o args does not trigger an error

--- types/types.test-d.ts
+++ types/types.test-d.ts
@@ -65,6 +65,7 @@ app.route({
     expectError((request.sessionStore = null));
     expectError(request.session.doesNotExist());
     expectType<{ id: number } | undefined>(request.session.user);
+    request.sessionStore.set('session-set-test', request.session, () => {})
     request.sessionStore.get('', (err, session) => {
       expectType<Error>(err);
       expectType<Session>(session);
