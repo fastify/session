388118c55cc0f104199579dde61b9717603f0dfc
Simen Bekkhus <sbekkhus91@gmail.com>
2022-03-25 11:41:30 +0100

    remove ifs

--- types/types.test-d.ts
+++ types/types.test-d.ts
@@ -69,8 +69,8 @@ app.route({
     expectError((request.sessionStore = null));
     expectError(request.session.doesNotExist());
     expectType<{ id: number } | undefined>(request.session.user);
-    if(request.session.regenerate) request.session.regenerate();
-    if(request.session.regenerate) request.session.regenerate(request);
+    request.session.regenerate();
+    request.session.regenerate(request);
     request.sessionStore.set('session-set-test', request.session, () => {})
     request.sessionStore.get('', (err, session) => {
       expectType<Error | null>(err);
