56f562b94bc4d1fc45c476cc50b0b51c35b1588d
Paolo Insogna <paolo@cowtech.it>
2022-05-19 16:47:11 +0200

    feat: Update benchmark. (#91)

--- bench.js
+++ bench.js
@@ -1,42 +1,42 @@
 'use strict'
 
-const cronometro = require('cronometro')
-const Fastify = require('fastify')
-const fastifyCookie = require('fastify-cookie')
-const fileStoreFactory = require('session-file-store')
 const redisStoreFactory = require('connect-redis')
-const { createClient: createRedisClient } = require('redis')
+const Fastify = require('fastify')
+const Redis = require('ioredis')
+const fileStoreFactory = require('session-file-store')
+const { isMainThread } = require('worker_threads')
+
 const fastifySession = require('.')
+const oldFastifyCookie = require('fastify-cookie')
 const oldFastifySession = require('published-session')
-const fastifyPlugin = require('fastify-plugin')
+const fastifyCookie = require('@fastify/cookie')
 
-const sessionPlugin = async (fastify, { plugin, storeType }) => {
-  const FileStore = fileStoreFactory(plugin)
-  const RedisStore = redisStoreFactory(plugin)
-  const redisClient = createRedisClient({ legacyMode: true })
-  // only run this if you have redis running on localhost:6379
-  await redisClient.connect()
+let redisClient
 
-  fastify.register(fastifyCookie)
-
-  fastify.register(plugin, {
-    secret: 'cNaoPYAwF60HZJzkcNaoPYAwF60HZJzk',
-    saveUninitialized: false,
-    cookie: { secure: false },
-    store: storeType === 'redis'
-      ? new RedisStore({ client: redisClient })
-      : storeType === 'file'
-        ? new FileStore({})
-        : undefined
-  })
-}
-
-const getServer = (plugin, storeType) => {
+function createServer (sessionPlugin, cookiePlugin, storeType) {
   let requestCounter = 0
+  let store
+
+  if (storeType === 'redis') {
+    const RedisStore = redisStoreFactory(sessionPlugin)
+    if (!redisClient) {
+      redisClient = new Redis()
+    }
+    store = new RedisStore({ client: redisClient })
+  } else if (storeType === 'file') {
+    const FileStore = fileStoreFactory(sessionPlugin)
+    store = new FileStore({})
+  }
 
   const fastify = Fastify()
 
-  fastify.register(fastifyPlugin(sessionPlugin), { plugin, storeType })
+  fastify.register(cookiePlugin)
+  fastify.register(sessionPlugin, {
+    secret: 'cNaoPYAwF60HZJzkcNaoPYAwF60HZJzk',
+    saveUninitialized: false,
+    cookie: { secure: false },
+    store
+  })
 
   fastify.get('/', (request, reply) => {
     // modify session every 10 requests
@@ -54,10 +54,10 @@ const getServer = (plugin, storeType) => {
   return fastify
 }
 
-const testFunction = (sessionPlugin, storeType) => {
-  const server = getServer(sessionPlugin, storeType)
+function testFunction (sessionPlugin, cookiePlugin, storeType) {
+  const server = createServer(sessionPlugin, cookiePlugin, storeType)
 
-  return async () => {
+  return async function () {
     const { headers } = await server.inject('/')
     const setCookieHeader = headers['set-cookie']
 
@@ -68,20 +68,45 @@ const testFunction = (sessionPlugin, storeType) => {
     const { sessionId } = server.parseCookie(setCookieHeader)
 
     // make 25 "requests" with the new session
-    await Promise.all(new Array(25).fill(0).map(() =>
-      server.inject({ path: '/', headers: { cookie: `sessionId=${sessionId}` } }))
+    await Promise.all(
+      new Array(1).fill(0).map(() => server.inject({ path: '/', headers: { cookie: `sessionId=${sessionId}` } }))
     )
   }
 }
 
-cronometro(
-  {
-    'current code (memory)': testFunction(fastifySession),
-    'old code (memory)': testFunction(oldFastifySession),
-    'current code (file)': testFunction(fastifySession, 'file'),
-    'old code (file)': testFunction(oldFastifySession, 'file'),
-    'current code (redis)': testFunction(fastifySession, 'redis'),
-    'old code (redis)': testFunction(oldFastifySession, 'redis')
-  },
-  { iterations: 25 }
-)
+async function main () {
+  const { default: cronometro } = await import('cronometro')
+
+  return cronometro(
+    {
+      'current code (memory)': testFunction(fastifySession, fastifyCookie),
+      'old code (memory)': testFunction(oldFastifySession, oldFastifyCookie),
+      'current code (file)': testFunction(fastifySession, fastifyCookie, 'file'),
+      'old code (file)': testFunction(oldFastifySession, oldFastifyCookie, 'file'),
+      'current code (redis)': {
+        test: testFunction(fastifySession, fastifyCookie, 'redis'),
+        async after () {
+          return redisClient.disconnect()
+        }
+      },
+      'old code (redis)': {
+        test: testFunction(oldFastifySession, oldFastifyCookie, 'redis'),
+        async after () {
+          return redisClient.disconnect()
+        }
+      }
+    },
+    {
+      iterations: 25,
+      print: { compare: true }
+    }
+  )
+}
+
+if (isMainThread) {
+  main()
+    .then(() => redisClient.quit())
+    .catch(error => console.error(error))
+} else {
+  module.exports = main
+}
