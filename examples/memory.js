8e86dd0ebab0544c2693e973bf4425b60c3527b6
Uzlopak <aras.abbasi@googlemail.com>
2022-08-16 08:52:24 +0200

    add autocannon example (#128)
    * add autocannon example
    
    * Update examples/memory.js
    
    Co-authored-by: Rafael Gonzaga <rafael.nunu@hotmail.com>
    
    Co-authored-by: Rafael Gonzaga <rafael.nunu@hotmail.com>

---
+++ examples/memory.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const Fastify = require('fastify')
+const fastifySession = require('..')
+const fastifyCookie = require('@fastify/cookie')
+
+const fastify = Fastify()
+
+fastify.register(fastifyCookie, {})
+fastify.register(fastifySession, {
+  secret: 'cNaoPYAwF60HZJzkcNaoPYAwF60HZJzk',
+  cookie: { secure: false }
+})
+
+fastify.get('/', (request, reply) => {
+  reply
+    .send(request.cookies.sessionId)
+})
+
+const response = fastify.inject('/')
+response.then(v => console.log(`
+
+autocannon -H "Cookie=${decodeURIComponent(v.headers['set-cookie'])}" http://127.0.0.1:3000`))
+
+fastify.listen({ port: 3000 })
