5f9f8d8499daf106b3acdcb5f82bcfbd542463b9
James Crosby <autopulated@users.noreply.github.com>
2024-04-18 11:51:46 +0100

    enhance checkOptions to reject invalid signer objects, and test. This catches accidentally passing a Buffer instead of a string. (#241)

--- test/fastifySession.checkOptions.test.js
+++ test/fastifySession.checkOptions.test.js
@@ -14,7 +14,7 @@ test('fastifySession.checkOptions: register should fail if no secret is specifie
   fastify.register(fastifyCookie)
   fastify.register(fastifySession, options)
 
-  await t.rejects(fastify.ready(), new Error('the secret option is required!'))
+  await t.rejects(fastify.ready(), new Error('the secret option is required, and must be a String, Array of Strings, or a signer object with .sign and .unsign methods'))
 })
 
 test('fastifySession.checkOptions: register should succeed if secret with 32 characters is specified', async t => {
@@ -72,4 +72,40 @@ test('fastifySession.checkOptions: register should fail if no secret is present 
   fastify.register(fastifyCookie)
   fastify.register(fastifySession, { secret: [] })
   await t.rejects(fastify.ready(), new Error('at least one secret is required'))
+})
+
+test('fastifySession.checkOptions: register should fail if a Buffer is passed', async t => {
+  t.plan(1)
+  const fastify = Fastify()
+
+  fastify.register(fastifyCookie)
+  fastify.register(fastifySession, { secret: crypto.randomBytes(32) })
+  await t.rejects(fastify.ready(), new Error('the secret option is required, and must be a String, Array of Strings, or a signer object with .sign and .unsign methods'))
+})
+
+test('fastifySession.checkOptions: register should fail if a signer missing unsign is passed', async t => {
+  t.plan(1)
+  const fastify = Fastify()
+
+  const invalidSigner = {
+    sign: (x) => x,
+    unsign: true
+  }
+
+  fastify.register(fastifyCookie)
+  fastify.register(fastifySession, { secret: invalidSigner })
+  await t.rejects(fastify.ready(), new Error('the secret option is required, and must be a String, Array of Strings, or a signer object with .sign and .unsign methods'))
+})
+
+test('fastifySession.checkOptions: register should fail if a signer missing sign is passed', async t => {
+  t.plan(1)
+  const fastify = Fastify()
+
+  const invalidSigner = {
+    unsign: (x) => true
+  }
+
+  fastify.register(fastifyCookie)
+  fastify.register(fastifySession, { secret: invalidSigner })
+  await t.rejects(fastify.ready(), new Error('the secret option is required, and must be a String, Array of Strings, or a signer object with .sign and .unsign methods'))
 })
