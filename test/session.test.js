2a0ffbed216406280a21e75a6c50035d59e4fe43
Oleg Gheorghita <ogheo@users.noreply.github.com>
2024-07-25 15:55:21 +0300

    Added the ability to configure cookie options within a route (#260)
    * Added the ability to configure cookie options within a route
    
    * Fixed usage of regenerate, preserve old cookieOpts
    
    Co-authored-by: Gürgün Dayıoğlu <hey@gurgun.day>
    Signed-off-by: Oleg Gheorghita <ogheo@users.noreply.github.com>
    
    * Updated override global options test with regenerate
    
    * Moved overriding global options using regenerate fn to a separate test
    
    ---------
    
    Signed-off-by: Oleg Gheorghita <ogheo@users.noreply.github.com>
    Co-authored-by: Gürgün Dayıoğlu <hey@gurgun.day>

--- test/session.test.js
+++ test/session.test.js
@@ -1083,3 +1083,191 @@ test('should save session if existing, modified, rolling false, and cookie.expir
   })
   t.equal(response3.statusCode, 200)
 })
+
+test('Custom options', async t => {
+  t.plan(6)
+
+  const fastify = Fastify()
+  fastify.register(fastifyCookie)
+  fastify.register(fastifySession, {
+    ...DEFAULT_OPTIONS,
+    cookie: {
+      secure: false,
+      path: '/'
+    }
+  })
+
+  fastify.post('/', (request, reply) => {
+    request.session.set('data', request.body)
+    request.session.options({ maxAge: 1000 * 60 * 60 })
+    reply.send('hello world')
+  })
+
+  t.teardown(fastify.close.bind(fastify))
+
+  fastify.get('/', (request, reply) => {
+    const data = request.session.get('data')
+    if (!data) {
+      reply.code(404).send()
+      return
+    }
+    reply.send(data)
+  })
+
+  fastify.inject({
+    method: 'POST',
+    url: '/',
+    payload: {
+      some: 'data'
+    }
+  }, (error, response) => {
+    t.error(error)
+    t.equal(response.statusCode, 200)
+    t.ok(response.headers['set-cookie'])
+    const { expires } = response.cookies[0]
+    t.equal(expires.toUTCString(), new Date(Date.now() + 1000 * 60 * 60).toUTCString())
+
+    fastify.inject({
+      method: 'GET',
+      url: '/',
+      headers: {
+        cookie: response.headers['set-cookie']
+      }
+    }, (error, response) => {
+      t.error(error)
+      t.same(JSON.parse(response.payload), { some: 'data' })
+    })
+  })
+})
+
+test('Override global options', t => {
+  t.plan(11)
+
+  const fastify = Fastify()
+  fastify.register(fastifyCookie)
+  fastify.register(fastifySession, {
+    ...DEFAULT_OPTIONS,
+    cookie: {
+      secure: false,
+      maxAge: 42,
+      path: '/'
+    }
+  })
+
+  fastify.post('/', (request, reply) => {
+    request.session.set('data', request.body)
+    request.session.options({ maxAge: 1000 * 60 * 60 })
+
+    reply.send('hello world')
+  })
+
+  t.teardown(fastify.close.bind(fastify))
+
+  fastify.get('/', (request, reply) => {
+    const data = request.session.get('data')
+
+    if (!data) {
+      reply.code(404).send()
+      return
+    }
+    reply.send(data)
+  })
+
+  fastify.inject({
+    method: 'POST',
+    url: '/',
+    payload: {
+      some: 'data'
+    }
+  }, (error, response) => {
+    t.error(error)
+    t.equal(response.statusCode, 200)
+    t.ok(response.headers['set-cookie'])
+    const { expires, path } = response.cookies[0]
+    t.equal(expires.toUTCString(), new Date(Date.now() + 1000 * 60 * 60).toUTCString())
+    t.equal(path, '/')
+
+    fastify.inject({
+      method: 'GET',
+      url: '/',
+      headers: {
+        cookie: response.headers['set-cookie']
+      }
+    }, (error, response) => {
+      t.error(error)
+      t.equal(response.statusCode, 200)
+      t.same(JSON.parse(response.payload), { some: 'data' })
+      t.ok(response.headers['set-cookie'])
+      const { expires, path } = response.cookies[0]
+      t.equal(expires.toUTCString(), new Date(Date.now() + 1000 * 60 * 60).toUTCString())
+      t.equal(path, '/')
+    })
+  })
+})
+
+test('Override global options with regenerate', t => {
+  t.plan(11)
+
+  const fastify = Fastify()
+  fastify.register(fastifyCookie)
+  fastify.register(fastifySession, {
+    ...DEFAULT_OPTIONS,
+    cookie: {
+      secure: false,
+      maxAge: 42,
+      path: '/'
+    }
+  })
+
+  fastify.post('/', (request, reply) => {
+    request.session.set('data', request.body)
+    request.session.options({ maxAge: 1000 * 60 * 60 }) // maxAge updated to 1 hour
+
+    reply.send('hello world')
+  })
+
+  t.teardown(fastify.close.bind(fastify))
+
+  fastify.get('/', async (request, reply) => {
+    const data = request.session.get('data')
+    await request.session.regenerate()
+
+    if (!data) {
+      reply.code(404).send()
+      return
+    }
+
+    reply.send(data)
+  })
+
+  fastify.inject({
+    method: 'POST',
+    url: '/',
+    payload: {
+      some: 'data'
+    }
+  }, (error, response) => {
+    t.error(error)
+    t.equal(response.statusCode, 200)
+    t.ok(response.headers['set-cookie'])
+    const { expires, path } = response.cookies[0]
+    t.equal(expires.toUTCString(), new Date(Date.now() + 1000 * 60 * 60).toUTCString())
+    t.equal(path, '/')
+
+    fastify.inject({
+      method: 'GET',
+      url: '/',
+      headers: {
+        cookie: response.headers['set-cookie']
+      }
+    }, (error, response) => {
+      t.error(error)
+      t.equal(response.statusCode, 200)
+      t.same(JSON.parse(response.payload), { some: 'data' })
+      t.ok(response.headers['set-cookie'])
+      const { expires, path } = response.cookies[0]
+      t.equal(expires.toUTCString(), new Date(Date.now() + 1000 * 60 * 60).toUTCString())
+      t.equal(path, '/')
+    })
+  })
+})
