c04713890b0f3f1792f5cc5265ce2b12738dca01
William Ferreira da Silva <williamfdasilva@gmail.com>
2025-05-29 21:56:19 -0300

    fix(tests): populate Set in idGenerator tests (#266)

--- test/idGenerator.test.js
+++ test/idGenerator.test.js
@@ -3,7 +3,7 @@
 const test = require('node:test')
 const idGenerator = require('../lib/idGenerator')
 
-const cacheSize = 1 << 7 + 1
+const cacheSize = (1 << 7) + 1
 
 if (Buffer.isEncoding('base64url')) {
   test('should have no collisions, base64url', async (t) => {
@@ -11,12 +11,13 @@ if (Buffer.isEncoding('base64url')) {
     t.plan(cacheSize)
     const ids = new Set()
 
-    for (let i = 0; i < (cacheSize); ++i) {
+    for (let i = 0; i < cacheSize; ++i) {
       const id = idGen()
       if (ids.has(id)) {
         t.assert.ifError('had a collision')
       }
       t.assert.strictEqual(id.length, 32)
+      ids.add(id)
     }
   })
 }
@@ -26,11 +27,12 @@ test('should have no collisions, base64', async (t) => {
   t.plan(cacheSize)
   const ids = new Set()
 
-  for (let i = 0; i < (cacheSize); ++i) {
+  for (let i = 0; i < cacheSize; ++i) {
     const id = idGen()
     if (ids.has(id)) {
       t.assert.ifError('had a collision')
     }
     t.assert.strictEqual(id.length, 32)
+    ids.add(id)
   }
 })
