f9779f4d69df0d83e43101b62ba1fc6e8d4af36a
William Ferreira da Silva <williamfdasilva@gmail.com>
2025-06-01 09:53:15 -0300

    fix: correct operator precedence in cacheSize definition

--- test/idGenerator.test.js
+++ test/idGenerator.test.js
@@ -3,7 +3,7 @@
 const test = require('node:test')
 const idGenerator = require('../lib/idGenerator')
 
-const cacheSize = (1 << 7) + 1
+const cacheSize = 1 << 7 + 1
 
 if (Buffer.isEncoding('base64url')) {
   test('should have no collisions, base64url', async (t) => {
